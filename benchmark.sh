f98879c5bfe579c762bdf29bf16cad152271df59
Ankur Dave <ankurdave@gmail.com>
2016-05-10 12:51:41 -0700

    Use leaf reordering for part benchmarks

--- benchmark.sh
+++ benchmark.sh
@@ -6,16 +6,17 @@ cd "$(dirname "$0")"
 
 paper_dir=~/repos/indexedrdd-paper/osdi16
 figure_dir=$paper_dir/figures
+date=$(date -u +"%Y-%m-%dT%H-%M-%S")
 
 CXX_OPTS='-Wall -Wextra -Wno-invalid-offsetof -O3 -std=c++11 -stdlib=libc++ -g'
 
 ####### Data structure performance comparison
-data_file=$figure_dir/micro-out.txt
-echo 'data=[' > $data_file
+data_file=$figure_dir/micro-out-$date.txt
+echo 'data += [' > $data_file
 
 for value_size in '' '-DBIG_VAL'; do
     for b in 1 10 100 1000 10000 100000 1000000 10000000; do
-        clang++ -DART -DBATCH_SIZE=$b $value_size -DKEY_LEN=4 -DRANDOM $CXX_OPTS -o ArtMicrobenchmark ArtMicrobenchmark.cpp
+        clang++ -DART -DART_REORDER_LEAVES -DBATCH_SIZE=$b $value_size -DKEY_LEN=4 -DRANDOM $CXX_OPTS -o ArtMicrobenchmark ArtMicrobenchmark.cpp
         ./ArtMicrobenchmark | tee -a $data_file
 
         clang++ -DHT -DBATCH_SIZE=$b $value_size -DKEY_LEN=4 -DRANDOM $CXX_OPTS -o ArtMicrobenchmark ArtMicrobenchmark.cpp
@@ -32,7 +33,6 @@ for value_size in '' '-DBIG_VAL'; do
     ./ArtMicrobenchmark | tee -a $data_file
 done
 
-
 echo ']' >> $data_file
 
 
@@ -42,8 +42,6 @@ echo 'stacked_data=[' > $data_file
 echo '# 1. No batching, no node compaction'
 clang++ -DART -DBATCH_SIZE=1 -DKEY_LEN=4 -DRANDOM $CXX_OPTS -o ArtMicrobenchmark ArtMicrobenchmark.cpp
 ./ArtMicrobenchmark | tee -a $data_file
-
-# 2. +Key space transformations (improves all)
 
 echo '# 3. +Batching, batch size=10,000 (improves insert performance)'
 clang++ -DART -DBATCH_SIZE=10000 -DKEY_LEN=4 -DRANDOM $CXX_OPTS -o ArtMicrobenchmark ArtMicrobenchmark.cpp
